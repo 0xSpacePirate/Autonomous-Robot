2f532097d80db7f6e6b28ee120c0396280de1c58
POBnH4 <bonchp4@yahoo.com>
2020-01-22 21:26:30 +0000

    Working servos, including them being able to turn left, right, and back

--- RobotScripts/Robot.py
+++ RobotScripts/Robot.py
@@ -1,1 +1,58 @@
-print("hello there")
+import RPi.GPIO as GPIO
+import time
+import LeftServo as leftMotor
+import RightServo as rightMotor
+from threading import Thread
+
+print("Launch Configuration initiated")
+
+
+
+def startMotors():
+	leftMotor.start()
+	rightMotor.start()
+	print("Motors started")
+
+
+def stopMotors():
+	leftMotor.stop()
+	rightMotor.stop()
+	GPIO.cleanup()
+	print("Motors stopped")
+
+
+def turnLeft():
+	left = 7.5 # turn towards 90 degrees
+	rightMotor.stop()
+	leftMotor.turn(left)
+
+
+def turnRight():
+	right = 7.5  # turn towards 90 degrees
+	leftMotor.stop()
+	rightMotor.turn(right)
+
+
+def turnBack():	
+	back = 12.5 # turn backwards 180 degrees
+	rightMotor.stop()
+	leftMotor.turn(12.5)
+
+
+startMotors()
+
+turnLeft()
+turnRight()
+turnBack()
+
+stopMotors()
+
+
+
+
+
+
+
+
+
+
---
+++ RobotScripts/robotLaunchConf.py
@@ -0,0 +1,15 @@
+import Robot as robot
+import PID as pid
+import RL as rl
+
+#Hardware
+#import GyroReader as gyroscope
+#import LeftServo as leftMotor
+#import RightServo as rightMotor
+
+
+
+
+
+
+print("Launch configuration executed")
